6d71fe166e39bd03d0500142feecb583d4ea60a2
pepe-moreno <esn2@cin.ufpe.br>
2025-08-06 10:55:42 -0300

    Create left_outer_join.py

---
+++ Projeto Físico/SQL/left_outer_join.py
@@ -0,0 +1,6 @@
+from execute_query import execute_query
+
+query = """ SELECT Treino.equipamento.ID_treino, TreinoExercício.ID_exercício
+FROM TreinoEquipamento
+LEFT JOIN TreinoEquipamento ON TreinoEquipamento.ID_treino = TreinoExercício.ID_treino
+"""
