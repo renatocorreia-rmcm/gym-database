0404f1f524e47a42942174b04bba8e600460dbee
renatocorreia-rmcm <rmcm@cin.ufpe.br>
2025-08-06 11:47:53 -0300

    union

---
+++ Projeto Físico/SQL/querys/union.py
@@ -0,0 +1,14 @@
+from execute_query import execute_query
+
+# Lista de CPFs de todos os envolvidos na academia (alunos ou funcionários).
+
+query = """ 
+SELECT CPF_func AS CPF
+FROM Funcionário
+UNION
+SELECT CPF_aluno
+FROM Aluno;
+
+"""
+
+execute_query(query)
