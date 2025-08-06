e82662b25824650e7e75def7de999992046756c6
duwuh <leal@cin.ufpe.br>
2025-08-05 22:47:29 -0300

    add projeto fisico unidade_equipamento

---
+++ Projeto Físico/(Luiz) Unidade_Equipamento/consultas.py
@@ -0,0 +1,48 @@
+import pymongo
+
+uri = "mongodb+srv://leal:BDleal@cluster0.hk2pult.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+cliente = pymongo.MongoClient(uri)
+bd = cliente["academia"]
+
+equipamento = bd["equipamento"]
+unidade = bd["unidade"]
+
+# CASO 1
+# Quais são as cidades das unidades que possuem o equipamento com o nome="Leg press"
+print("Caso 1:")
+equipamentos_leg_press = equipamento.find({"nome": "Leg press"})
+
+for eq in equipamentos_leg_press:
+    unidade_doc = unidade.find_one({"_id": eq["id_unidade"]})
+    print(unidade_doc["cidade"])
+
+# CASO 2
+# Quais sao os nomes dos equipamentos da unidade da cidade ="São Paulo"
+print()
+print("Caso 2:")
+equipamento_sao_paulo = equipamento.find({"unidade.cidade": "São Paulo"})
+
+for eq in equipamento_sao_paulo:
+    print(eq["nome"])
+
+#Caso 3
+#Quais são os nomes dos equipamentos da unidade da cidade = "Fortaleza"
+print()
+print("Caso 3:")
+unidade_fortaleza = unidade.find_one({"cidade": "Fortaleza"})
+
+for equip_id in unidade_fortaleza["equipamentos"]:
+    eq = equipamento.find_one({"_id": equip_id})
+    if eq:
+        print(eq["nome"])
+
+
+# CASO 4
+# Quais são os nomes dos equipamentos da unidade da cidade = "Rio de Janeiro"
+print()
+print("Caso 4:")
+unidade_rio = unidade.find_one({"cidade": "Rio de Janeiro"})
+
+for eq in unidade_rio["equipamentos"]:
+    print(eq["nome"])
---
+++ Projeto Físico/(Luiz) Unidade_Equipamento/inserir.py
@@ -0,0 +1,77 @@
+import pymongo
+
+uri = "mongodb+srv://leal:BDleal@cluster0.hk2pult.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+cliente = pymongo.MongoClient(uri)
+
+bd = cliente['academia']
+unidade = bd['unidade']
+equipamento = bd['equipamento']
+
+# CASO 1 - Um documento (unidade) referenciando outro documento (equipamento)
+userId = unidade.insert_one({
+    "_id": 1,
+    "cidade": "Recife",
+    "CEP": "12345-678"
+}).inserted_id
+
+equipamento.insert_one({
+    "_id": 1,
+    "nome": "Leg press",
+    "quantidade": 3,
+    "id_unidade": userId
+})
+
+#CASO 2 - Um documento (equipamento) embutindo apenas um outro documento (unidade)
+unidade_sao_paulo = {
+    "_id": 2,
+    "cidade": "São Paulo",
+    "CEP": "11111-222"
+}
+unidade.insert_one(unidade_sao_paulo)
+
+equipamento.insert_one({
+    "_id": 2,
+    "nome": "Esteira",
+    "quantidade": 12,
+    "unidade": unidade_sao_paulo 
+})
+
+# CASO 3 - Um documento (unidade) com um array de referências para documentos de equipamentos
+equip1_id = equipamento.insert_one({
+    "_id": 3,
+    "nome": "Bicicleta",
+    "quantidade": 5
+}).inserted_id
+
+equip2_id = equipamento.insert_one({
+    "_id": 4,
+    "nome": "Remo",
+    "quantidade": 2
+}).inserted_id
+
+unidade.insert_one({
+    "_id": 3,
+    "cidade": "Fortaleza",
+    "CEP": "22222-333",
+    "equipamentos": [equip1_id, equip2_id]  # Array de referências
+})
+
+# CASO 4 - um documento (unidade) embuntindo varios documentos (equipamento)
+equip_list = [{
+    "_id": 5,
+    "nome": "Halter",
+    "quantidade": 40,
+},{
+    "_id": 6,
+    "nome": "Barra fixa",
+    "quantidade": 8,
+}]
+equipamento.insert_many(equip_list)
+
+unidade.insert_one({
+    "_id": 4,
+    "cidade": "Rio de Janeiro",
+    "CEP": "77777-888",
+    "equipamentos": equip_list
+})
